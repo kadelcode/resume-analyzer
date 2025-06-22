33bfb60dac136620c6e2434feeac3d9f0fc2515d
kadelcode <kadelcode@gmail.com>
2025-06-22 06:49:27 +0100

    Include components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Upload from "@/components/Upload";
 import ValueProposition from "@/components/ValueProposition";
+import Testimonials from "@/components/Testimonials";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[url('/patterns/connecting-dots.svg')] bg-[length:80px_80px] bg-fixed">
       <div 
-        className="backdrop-blur-xs py-12"
+        className="backdrop-blur-xs"
         style={{backdropFilter:'blur(4px)'}}
       >
+        {/* Header */}
+        <Header />
+
         {/* Hero Section */}
         <Hero />
 
@@ -17,6 +23,12 @@ export default function Home() {
         
         {/* Upload Section */}
         <Upload />
+
+        {/* Testimonial Section */}
+        <Testimonials />
+
+        {/* Footer */}
+        <Footer />
       </div>
     </main>
   )
